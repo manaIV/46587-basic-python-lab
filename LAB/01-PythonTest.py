61e064e07110cc4d0df547266d121ca2f1ea274a
manaIV <164491350+manaIV@users.noreply.github.com>
2025-06-04 10:02:12 +0700

    4/05 basisc python

--- LAB/01-PythonTest.py
+++ LAB/01-PythonTest.py
@@ -1,3 +1,3 @@
 print ("What is your favorite song")
-song = input ()
+song = input ("type ur song")
 print("cool! I like", song , "too")
---
+++ LAB/02-PythonInputOutput.py
@@ -0,0 +1,4 @@
+name = input("What is your name? : ")
+like = input("Whay do you like? : ")
+
+print(name + " likes " + like + ".")
